cbfa2db6379db1ce19bced5ef36eb732aaf94402
Nicolas <n.jandard@gmail.com>
2017-01-17 16:42:16 +0100

    test conversion

--- src/main/java/org/insset/server/RomanConverterServiceImpl.java
+++ src/main/java/org/insset/server/RomanConverterServiceImpl.java
@@ -15,6 +15,11 @@ import org.insset.client.service.RomanConverterService;
 @SuppressWarnings("serial")
 public class RomanConverterServiceImpl extends RemoteServiceServlet implements
         RomanConverterService {
+    
+    private final int[] nombres = {1000, 500, 100, 50, 10, 5, 1 };
+    private final int MAX = 2000;
+    private final String symboles = "MDCLXVI";
+    private String decimaux;
 
     @Override
     public String convertDateYears(String nbr) throws IllegalArgumentException {
@@ -24,14 +29,98 @@ public class RomanConverterServiceImpl extends RemoteServiceServlet implements
 
     @Override
     public Integer convertRomanToArabe(String nbr) throws IllegalArgumentException {
-        //Implement your code
-        return 3;
+        String rom = "";
+        boolean bool = false;
+        int x=0;
+        int p=0;
+        
+        //System.out.print("Entrez un nombre en chiffres romains : ");
+        
+        nbr=nbr.intern();
+        nbr=nbr.toUpperCase();
+        /*System.out.println("La valeur entre "+chaine);*/
+        
+        
+        for(int i=0; i<nbr.length(); ++i)
+        {
+           this.decimaux=nbr.substring(i,i+1);
+           
+              for(int j=0; j<this.symboles.length();j++)
+              {
+                   rom=this.symboles.substring(j,j+1);
+                   
+                   if(this.decimaux.equals(rom)) {bool=true;break;}
+                   else {bool=false;}
+              }
+              
+          if(bool==false){/*System.out.println("Conversion impossible, nombre romain mal formé.");*/break;}
+        }
+        
+        if(bool)
+        {
+            for(int i=0; i<nbr.length(); ++i)
+            {
+              this.decimaux=nbr.substring(i,i+1);
+              for(int j=0; j<this.symboles.length();j++)
+              {
+                  rom=this.symboles.substring(j,j+1);
+                   if(this.decimaux.equals(rom))
+                   { if(j>=p){x+=this.nombres[j];
+                                p=j;
+                         }else{
+                               x-=(2*this.nombres[p]-this.nombres[j]);
+                               p=j;}
+                     }
+                }
+            }
+          //System.out.println("décimaux("+chaine+") = "+x);
+        }
+        return x;
     }
 
     @Override
     public String convertArabeToRoman(Integer nbr) throws IllegalArgumentException {
-        //Implement your code
-        return new String("XVXX");
-    }
+        int temp=nbr;
+        int k=0;
+        String conversion="";
+        //System.out.print("Entrez un nombre (en chiffres décimaux) compris entre 1 et " + MAX + " : "); 
+        
+        /*System.out.println("La valeur entre "+chaine);*/
+        /*Conversion*/
+        for(int i=0;i< this.nombres.length;++i)
+        {
+            if(temp==4)
+            {
+                temp=0;
+                conversion=conversion+"IV";
+            }
+            while(temp>=this.nombres[i] && k<3)
+            {
+                ++k;
+                temp=temp-this.nombres[i];
+                conversion=conversion+this.symboles.charAt(i);
+            }
+            if(i <this.nombres.length-1)
+            {
+               if(temp/this.nombres[i+1]>1 && (((temp/this.nombres[i+1])*this.nombres[i+1])+this.nombres[i+1])==this.nombres[i])
+                {   
+                    temp=temp-(this.nombres[i]-this.nombres[i+1]);
+                    conversion=conversion+this.symboles.charAt(i+1)+this.symboles.charAt(i);
+                }
+            }
+            if(i<this.nombres.length-2)
+            {
+                if(temp/this.nombres[i+2]>1 && (((temp/this.nombres[i+2])*this.nombres[i+2])+this.nombres[i+2])==this.nombres[i])
+                { 
+                    temp=temp-(this.nombres[i]-this.nombres[i+2]);
+                    conversion=conversion+this.symboles.charAt(i+2)+this.symboles.charAt(i);
+                }
+            }
+            k=0;
+            if(temp==0){break;}
+        }
+        //System.out.println("romains("+nbr+") = "+conversion);
+        return conversion;
+    } 
 
 }
